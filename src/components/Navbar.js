59d3669f985f58b43275f86ba7370c8822e45722
Zack Ballard <zackballard27@yahoo.com>
2025-02-09 23:40:15 -0600

    Updated StreamList.js, Navbar.js, App.css, and added icons

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,17 +1,58 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import streamlistIcon from "../icons/streamlist.png";
+import aboutIcon from "../icons/about.png";
+import moviesIcon from "../icons/movies.png";
+import cartIcon from "../icons/shoppingcartcheckout.png";
 
 const Navbar = () => {
   return (
     <nav>
       <ul>
-        <li><Link to="/">StreamList</Link></li>
-        <li><Link to="/movies">Movies</Link></li>
-        <li><Link to="/cart">Cart</Link></li>
-        <li><Link to="/about">About</Link></li>
+        <li>
+          <Link to="/">
+            <img
+              src={streamlistIcon}
+              alt="StreamList"
+              style={{ width: "20px", height: "20px", marginRight: "5px" }}
+            />
+            StreamList
+          </Link>
+        </li>
+        <li>
+          <Link to="/movies">
+            <img
+              src={moviesIcon}
+              alt="Movies"
+              style={{ width: "20px", height: "20px", marginRight: "5px" }}
+            />
+            Movies
+          </Link>
+        </li>
+        <li>
+          <Link to="/cart">
+            <img
+              src={cartIcon}
+              alt="Cart"
+              style={{ width: "20px", height: "20px", marginRight: "5px" }}
+            />
+            Cart
+          </Link>
+        </li>
+        <li>
+          <Link to="/about">
+            <img
+              src={aboutIcon}
+              alt="About"
+              style={{ width: "20px", height: "20px", marginRight: "5px" }}
+            />
+            About
+          </Link>
+        </li>
       </ul>
     </nav>
   );
 };
 
 export default Navbar;
+
--- src/components/StreamList.js
+++ src/components/StreamList.js
@@ -1,30 +1,82 @@
 import React, { useState } from "react";
+import searchIcon from "../icons/search.png"; // New search icon
+import playIcon from "../icons/play.png"; // New play icon
+import deleteIcon from "../icons/delete.png";
+import editIcon from "../icons/edit.png"; // Updated edit icon
+import favoriteIcon from "../icons/favorite.png";
 
 const StreamList = () => {
   const [input, setInput] = useState("");
   const [list, setList] = useState([]);
 
+  // Add a new item to the list
   const addToList = () => {
     if (input.trim()) {
-      setList([...list, input]);
-      console.log("Added:", input);
-      setInput("");
+      setList([...list, { text: input, completed: false }]);
+      setInput(""); // Clear input field
     }
+  };
+
+  // Toggle item completion
+  const toggleComplete = (index) => {
+    const updatedList = [...list];
+    updatedList[index].completed = !updatedList[index].completed;
+    setList(updatedList);
+  };
+
+  // Edit an item
+  const editItem = (index) => {
+    const newText = prompt("Edit the item:", list[index].text);
+    if (newText) {
+      const updatedList = [...list];
+      updatedList[index].text = newText;
+      setList(updatedList);
+    }
+  };
+
+  // Delete an item
+  const deleteItem = (index) => {
+    setList(list.filter((_, i) => i !== index));
   };
 
   return (
     <div>
       <h1>StreamList</h1>
-      <input
-        type="text"
-        placeholder="Enter movie or show"
-        value={input}
-        onChange={(e) => setInput(e.target.value)}
-      />
-      <button onClick={addToList}>Add</button>
+      <div>
+        <input
+          type="text"
+          placeholder="Enter movie or show"
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+        />
+        <button onClick={addToList}>
+          <img src={searchIcon} alt="Search" style={{ width: "20px", height: "20px" }} />
+        </button>
+      </div>
       <ul>
         {list.map((item, index) => (
-          <li key={index}>{item}</li>
+          <li key={index} style={{ display: "flex", alignItems: "center", marginBottom: "10px" }}>
+            <span
+              style={{
+                textDecoration: item.completed ? "line-through" : "none",
+                flexGrow: 1,
+              }}
+            >
+              {item.text}
+            </span>
+            <button>
+              <img src={playIcon} alt="Play" style={{ width: "20px", height: "20px" }} />
+            </button>
+            <button onClick={() => toggleComplete(index)}>
+              <img src={favoriteIcon} alt="Complete" style={{ width: "20px", height: "20px" }} />
+            </button>
+            <button onClick={() => editItem(index)}>
+              <img src={editIcon} alt="Edit" style={{ width: "20px", height: "20px" }} />
+            </button>
+            <button onClick={() => deleteItem(index)}>
+              <img src={deleteIcon} alt="Delete" style={{ width: "20px", height: "20px" }} />
+            </button>
+          </li>
         ))}
       </ul>
     </div>
